0f30c853660a362ecab682f84d17a953f24e2d71
Keshav Jha <jhak78022@gmail.com>
2023-10-23 20:08:30 +0530

    updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,19 +5,7 @@ export default {
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {
-            colors: {
-                offWhite: 'hsl(0, 0%, 94%)',
-                offBlack: 'hsl(0, 0%, 13%)',
-                tomato: 'hsl(9, 100%, 70%)',
-                blue: 'hsl(195, 100%, 50%)',
-                green: 'hsl(150, 100%, 50%)',
-                fuchsia: 'hsl(300, 100%, 65%)',
-                paleYellow: 'hsl(60, 100%, 75%)',
-                lightGray: 'hsl(0, 0%, 85%)',
-                darkGray: 'hsl(0, 0%, 20%)',
-            }
-        },
+        extend: {},
     },
     plugins: [],
 }
