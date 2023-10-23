378bde0ec6f919df8d6aac309ad5c14c424c8165
Keshav Jha <jhak78022@gmail.com>
2023-10-23 19:48:28 +0530

    updated taliwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                offWhite: 'hsl(0, 0%, 94%)',
+                offBlack: 'hsl(0, 0%, 13%)',
+                tomato: 'hsl(9, 100%, 70%)',
+                blue: 'hsl(195, 100%, 50%)',
+                green: 'hsl(150, 100%, 50%)',
+                fuchsia: 'hsl(300, 100%, 65%)',
+                paleYellow: 'hsl(60, 100%, 75%)',
+                lightGray: 'hsl(0, 0%, 85%)',
+                darkGray: 'hsl(0, 0%, 20%)',
+            }
+        },
     },
     plugins: [],
 }
