def590e6c3bf88b3362a15bb1119619fd311eccf
Keshav Jha <jhak78022@gmail.com>
2023-10-23 20:08:11 +0530

    Updated Disclaimer component

--- src/components/Disclaimer.tsx
+++ src/components/Disclaimer.tsx
@@ -3,13 +3,9 @@ import { Link } from 'react-router-dom';
 
 const Disclaimer: React.FC = () => {
   return (
-    <div className='max-w-[800px] mx-auto rounded-md p-8 text-gray-700 bg-white shadow-md'>
+    <div className="max-w-[800px] mx-auto rounded-md p-8 text-gray-700 bg-blue-100 shadow-lg z-10 text-[1.1rem]">
       Your secrets are generated on this device with a{' '}
-      <Link
-        to="https://developer.mozilla.org/en-US/docs/Web/API/Crypto/getRandomValues"
-        title="getRandomValues docs on Mozilla.org"
-        className="underline text-blue-700 hover:text-blue-500"
-      >
+      <Link to="https://developer.mozilla.org/en-US/docs/Web/API/Crypto/getRandomValues" title="getRandomValues docs on Mozilla.org" className='underline text-blue-700 hover:text-blue-500'>
         cryptographically strong random number generator
       </Link>{' '}
       and are not transmitted or persisted in any way. Just look out for nosy parkers and
